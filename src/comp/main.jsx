167c0d2f4c8b2a6c52b6ddeff62665cc552e7b96
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-02 19:48:36 +0430

    changing in style

--- src/comp/main.jsx
+++ src/comp/main.jsx
@@ -5,7 +5,7 @@ const Main = () => {
         className="body w-100 d-flex flex-column justify-content-center align-items-center rounded"
         style={{ backgroundImage: "url(/images/back.jpeg)" }}
       >
-        <div className="text-in-main w-100 h-100 d-flex flex-column justify-content-center align-items-center">
+        <div className="text-in-main w-100 h-100 d-flex flex-column justify-content-center align-items-center text-center">
           <h1 className="" style={{ fontSize: "70px" }}>
             Welcome to our Cafe
           </h1>
