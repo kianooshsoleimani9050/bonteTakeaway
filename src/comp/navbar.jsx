63027ce8fe07c18cb38c4dc2a5134f09c7c31848
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-02 11:47:28 +0430

    add icon

--- src/comp/navbar.jsx
+++ src/comp/navbar.jsx
@@ -3,9 +3,19 @@ import { Link } from "react-router-dom";
 const Navbar = () => {
   return (
     <div className="p-0 m-0  navbar d-flex  justify-content-between align-items-center">
-      <h3 className="p-2 m-0 col-md-4 d-flex justify-content-center align-items-center">
-        Bonte Takeaway
-      </h3>
+      <div className="p-2 m-0 col-md-4 d-flex  justify-content-center align-items-center">
+        <i
+          className="navbar-icon"
+          style={{
+            backgroundImage: "url(/images/navbarowl.png)",
+          }}
+        ></i>
+        <h3 className="p-2 m-0  d-flex justify-content-center align-items-center">
+          Bonte Takeaway
+        </h3>
+      </div>
+      <div className="col-md-4"></div>
+
       <ul className="p-2 m-0 col-md-4 d-flex justify-content-center align-items-center">
         <Link className="link text-decoration-none" to="/">
           <li className="p-0 m-3">Home</li>
