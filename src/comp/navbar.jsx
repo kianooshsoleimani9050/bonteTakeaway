71c9d0368a1c08158aa9ab8ce8c5f61a1cafff2c
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-02 19:48:49 +0430

    changing for style

--- src/comp/navbar.jsx
+++ src/comp/navbar.jsx
@@ -1,32 +1,89 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { FaCoffee } from "react-icons/fa";
 const Navbar = () => {
+  const [show, setShow] = useState(false);
   return (
-    <div className="p-0 m-0  navbar d-flex  justify-content-between align-items-center">
-      <div className="p-2 m-0 col-md-4 d-flex  justify-content-center align-items-center">
-        <i
-          className="navbar-icon"
-          style={{
-            backgroundImage: "url(/images/navbarowl.png)",
-          }}
-        ></i>
-        <h3 className="p-2 m-0  d-flex justify-content-center align-items-center">
-          Bonte Takeaway
-        </h3>
-      </div>
-      <div className="col-md-4"></div>
+    <>
+      <div className="p-0 m-0  navbar d-flex  justify-content-between align-items-center">
+        <div className="p-2 m-0 col-md-4 col-sm-6 col-6 d-flex  justify-content-center align-items-center">
+          <i className="p-0 m-0">
+            <img
+              src="/images/navbarowl.png"
+              alt="bonte"
+              className="navbar-icon"
+            />
+          </i>
+          <h3 className="p-1 m-0  d-flex justify-content-center align-items-center">
+            Bonte Takeaway
+          </h3>
+        </div>
+        <div className="col-md-4 d-none d-md-flex"></div>
 
-      <ul className="p-2 m-0 col-md-4 d-flex justify-content-center align-items-center">
-        <Link className="link text-decoration-none" to="/">
-          <li className="p-0 m-3">Home</li>
-        </Link>
-        <Link className="link text-decoration-none" to="/menu">
-          <li className="p-0 m-3">Menu</li>
-        </Link>
-        <li className="p-0 m-3">About </li>
-        <li className="p-0 m-3">Contact Us</li>
-      </ul>
-    </div>
+        <ul className="p-1 m-0 col-md-4 col-sm-6 d-none d-sm-flex justify-content-center align-items-center">
+          <Link className="link text-decoration-none" to="/">
+            <li className="p-0 m-3">Home</li>
+          </Link>
+          <Link className="link text-decoration-none" to="/menu">
+            <li className="p-0 m-3">Menu</li>
+          </Link>
+          <li className="p-0 m-3">About </li>
+          <li className="p-0 m-3">Contact us</li>
+        </ul>
+        <div className="d-flex flex-column d-sm-none justiy-content-center align-items-center">
+          <i className="p-0 m-3">
+            <FaCoffee
+              className="navbar-icon"
+              onClick={() => {
+                setShow(!show);
+              }}
+            />
+          </i>
+        </div>
+      </div>
+      <div
+        className={`navbar-menu w-100 ${show ? "d-flex" : "d-none"} d-sm-none `}
+      >
+        <ul className="p-3 m-0 w-100">
+          <Link className="link text-decoration-none" to="/">
+            <li
+              className="li-hidden-menu p-1 m-0 mb-2 rounded w-100"
+              onClick={() => {
+                setShow(false);
+              }}
+            >
+              Home
+            </li>
+          </Link>
+          <Link className="link text-decoration-none" to="/menu">
+            <li
+              className="li-hidden-menu p-1 m-0 mb-2 w-100 rounded"
+              onClick={() => {
+                setShow(false);
+              }}
+            >
+              Menu
+            </li>
+          </Link>
+          <li
+            className="li-hidden-menu p-1 m-0 mb-2 w-100 rounded"
+            onClick={() => {
+              setShow(false);
+            }}
+          >
+            About{" "}
+          </li>
+          <li
+            className="li-hidden-menu p-1 m-0 mb-2 w-100 rounded"
+            onClick={() => {
+              setShow(false);
+            }}
+          >
+            Contact us
+          </li>
+        </ul>
+      </div>
+    </>
   );
 };
 
