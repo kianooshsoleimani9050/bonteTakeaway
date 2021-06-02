fa9ca0e68b0009e31f0202f87cb1df12f871be92
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-02 10:20:44 +0430

    add router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
-import React from "react"
-import Main from "./comp/main"
-
-
+import React from "react";
+import RouterPage from "./comp/route/router";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Navbar from "./comp/navbar";
+import Footer from "./comp/footer";
 function App() {
   return (
-    <div className="main">
-      <Main/>
-    </div>
-    
+    <>
+      <Router>
+        <Navbar />
+        <Switch>
+          <div className="main">
+            <RouterPage />
+          </div>
+        </Switch>
+        <Footer />
+      </Router>
+    </>
   );
 }
 
