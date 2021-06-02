4054dec431e2c0dc6c3f54d63d072f4bb58bee2e
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-02 19:48:22 +0430

    changing in style

--- src/comp/footer.jsx
+++ src/comp/footer.jsx
@@ -2,7 +2,7 @@ import { FaInstagram, FaFacebook, FaTwitter } from "react-icons/fa";
 const Footer = () => {
   return (
     <div className="footer p-0 m-0 d-flex flex-row hustify-content-center align-items-center">
-      <div className="p-0 m-0 col-md-6 d-flex flex-row justify-content-center align-items-center">
+      <div className="p-0 m-0 col-md-6 col-sm-6 col-6 d-flex flex-row justify-content-center align-items-center">
         <ul className="p-0 m-0 d-flex flex-row justify-content-center align-items-center">
           <li className="social-icon p-0 m-4 ">
             <FaInstagram />
@@ -15,8 +15,8 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <div className="p-0 m-0 col-md-6 d-flex flex-column  justify-content-center align-items-center">
-        <h5>Our address: </h5>
+      <div className="p-0 m-0 col-md-6 col-sm-6 col-6 d-flex flex-column  justify-content-center align-items-center">
+        <h5>Address: </h5>
         <h6>Shiraz kasayi st.</h6>
       </div>
     </div>
