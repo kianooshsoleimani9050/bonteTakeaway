bb1a48a83f43a9aa9bf5f72ecec57ecf9796c3ce
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-03 23:22:50 +0430

    change style

--- src/comp/navbar.jsx
+++ src/comp/navbar.jsx
@@ -6,19 +6,18 @@ const Navbar = () => {
   return (
     <>
       <div className="p-0 m-0  navbar d-flex  justify-content-between align-items-center">
-        <div className="p-2 m-0 col-md-4 col-sm-6 col-8 d-flex  justify-content-center align-items-center">
-          <i className="p-0 m-0">
+        <div className="p-2 m-0 col-md-4 col-sm-6 col-8 d-flex  justify-content-start align-items-center">
+          <i className="p-0 m-1">
             <img
               src="/images/navbarowl.png"
               alt="bonte"
               className="navbar-icon"
             />
           </i>
-          <h3 className="logo-name p-1 m-0  d-flex justify-content-center align-items-center">
+          <h3 className="logo-name p-1 m-0  d-flex justify-content-center align-items-start">
             Bonte Takeaway
           </h3>
         </div>
-        <div className="col-md-4 d-none d-md-flex"></div>
 
         <ul className="p-1 m-0 col-md-4 col-sm-6 d-none d-sm-flex justify-content-center align-items-center">
           <Link className="link text-decoration-none" to="/">
@@ -30,6 +29,7 @@ const Navbar = () => {
           <li className="p-0 m-3">About </li>
           <li className="p-0 m-3">Contact us</li>
         </ul>
+        <div className="col-md-4 d-none d-md-flex"></div>
         <div className="d-flex flex-column col-4 d-sm-none justiy-content-center align-items-end">
           <i className="p-0 m-3">
             <FaCoffee
