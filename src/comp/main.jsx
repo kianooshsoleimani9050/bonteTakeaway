5ada85696d4521a238f832b198a14ab027168c81
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-03 23:22:32 +0430

    chang style

--- src/comp/main.jsx
+++ src/comp/main.jsx
@@ -2,12 +2,12 @@ import { Link } from "react-router-dom";
 
 const Main = () => {
   return (
-    <div className="body p-0 m-0 d-flex flex-column justify-content-center align-items-center">
+    <div className="p-0 m-0 d-flex flex-column justify-content-center align-items-center">
       <div
-        className="body w-100 d-flex flex-column justify-content-center align-items-center rounded"
+        className="body d-flex flex-column justify-content-center align-items-center rounded"
         style={{ backgroundImage: "url(/images/back.jpeg)" }}
       >
-        <div className="text-in-main w-100 h-100 d-flex flex-column justify-content-center align-items-center text-center">
+        <div className="text-in-main d-flex flex-column justify-content-center align-items-center text-center">
           <h1 className="opening-title">Welcome to our Cafe</h1>
           <h3 className="">We serve you caffe and enjoy</h3>
           <button
