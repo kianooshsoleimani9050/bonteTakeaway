dad0e84186eb8018902eedc0d6f69e28f683fe96
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-04 22:15:13 +0430

    make this for about page content

---
+++ src/utils/about.js
@@ -0,0 +1,39 @@
+export const about = {
+  about_section: {
+    header: "",
+    content: "",
+  },
+  about_team: {
+    header: "",
+    content: "",
+  },
+  team: [
+    {
+      name: "Sina blurie",
+      post: "barista",
+      image: "",
+    },
+    {
+      name: "Sourosh dokohaki",
+      post: "barista",
+      image: "",
+    },
+    {
+      name: "Armin ariannia",
+      post: "barista",
+      image: "",
+    },
+
+    {
+      name: "Reza sahrayian",
+      post: "barista",
+      image: "",
+    },
+
+    {
+      name: "Reza dokohaki",
+      post: "barista",
+      image: "",
+    },
+  ],
+};
