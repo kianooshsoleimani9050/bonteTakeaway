2725eaa2bdaca7d25c98bf66265c1195b5f988c7
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-04 15:48:34 +0430

    add menu items

---
+++ src/utils/menu_items.js
@@ -0,0 +1,94 @@
+export const menuItem = [
+  {
+    name: "chemex",
+    price: "28 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622804379/WhatsApp_Image_2021-06-04_at_14.45.53_ohv5x8.jpg",
+  },
+  {
+    name: "Oreo shake",
+    price: "30 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799244/WhatsApp_Image_2021-06-04_at_14.00.31_obhxdw.jpg",
+  },
+  {
+    name: "V60",
+    price: "25 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622804373/WhatsApp_Image_2021-06-04_at_14.45.50_ksokhq.jpg",
+  },
+  {
+    name: "Glits",
+    price: "20 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799241/WhatsApp_Image_2021-06-04_at_14.00.30_gpkcid.jpg",
+  },
+  {
+    name: "Happy",
+    price: "25 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622804377/WhatsApp_Image_2021-06-04_at_14.45.54_canzif.jpg",
+  },
+  {
+    name: "Late 60/40",
+    price: "21 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799245/WhatsApp_Image_2021-06-04_at_14.00.29_blqibt.jpg",
+  },
+  {
+    name: "Late 100",
+    price: "24 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799245/WhatsApp_Image_2021-06-04_at_14.00.29_blqibt.jpg",
+  },
+  {
+    name: "Red shake",
+    price: "32 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799246/WhatsApp_Image_2021-06-04_at_14.00.34_jb07xe.jpg",
+  },
+  {
+    name: "Fizer",
+    price: "25 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799245/WhatsApp_Image_2021-06-04_at_14.00.32_bucfil.jpg",
+  },
+  {
+    name: "Mohito",
+    price: "25 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799245/WhatsApp_Image_2021-06-04_at_14.00.34_1_vngsqj.jpg",
+  },
+  {
+    name: "Watermelone juice ",
+    price: "24 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799246/WhatsApp_Image_2021-06-04_at_14.00.35_fjehpo.jpg",
+  },
+  {
+    name: "Adas",
+    price: "28 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799244/WhatsApp_Image_2021-06-04_at_14.00.33_rfu9x4.jpg",
+  },
+  {
+    name: "Red brew",
+    price: "25 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799244/WhatsApp_Image_2021-06-04_at_14.00.32_1_nlot6j.jpg",
+  },
+
+  {
+    name: "Cold brew",
+    price: "22 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799241/WhatsApp_Image_2021-06-04_at_14.00.30_1_u9qzuq.jpg",
+  },
+
+  {
+    name: "Chocolate shake",
+    price: "27 T",
+    image:
+      "https://res.cloudinary.com/bonte/image/upload/v1622799241/WhatsApp_Image_2021-06-04_at_14.00.23_zlalce.jpg",
+  },
+];
