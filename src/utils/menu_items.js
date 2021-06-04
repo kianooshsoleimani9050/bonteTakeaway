813f1fa76d44a498e98ce44fce7f5479471f2131
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-04 22:14:46 +0430

    add category

--- src/utils/menu_items.js
+++ src/utils/menu_items.js
@@ -2,78 +2,91 @@ export const menuItem = [
   {
     name: "chemex",
     price: "28 T",
+    category: "brewed_coffee",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622804379/WhatsApp_Image_2021-06-04_at_14.45.53_ohv5x8.jpg",
   },
   {
     name: "Oreo shake",
     price: "30 T",
+    category: "shake",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799244/WhatsApp_Image_2021-06-04_at_14.00.31_obhxdw.jpg",
   },
   {
     name: "V60",
     price: "25 T",
+    category: "brewed_coffee",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622804373/WhatsApp_Image_2021-06-04_at_14.45.50_ksokhq.jpg",
   },
   {
     name: "Glits",
     price: "20 T",
+    category: "mocktail",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799241/WhatsApp_Image_2021-06-04_at_14.00.30_gpkcid.jpg",
   },
   {
     name: "Happy",
     price: "25 T",
+    category: "hottie_without_coffee",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622804377/WhatsApp_Image_2021-06-04_at_14.45.54_canzif.jpg",
   },
   {
-    name: "Late 60/40",
+    name: "Latte 60/40",
     price: "21 T",
+    category: "espresso_based_coffee",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799245/WhatsApp_Image_2021-06-04_at_14.00.29_blqibt.jpg",
   },
   {
-    name: "Late 100",
+    name: "Latte 100",
     price: "24 T",
+    category: "espresso_based_coffee",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799245/WhatsApp_Image_2021-06-04_at_14.00.29_blqibt.jpg",
   },
   {
     name: "Red shake",
     price: "32 T",
+    category: "shake",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799246/WhatsApp_Image_2021-06-04_at_14.00.34_jb07xe.jpg",
   },
   {
     name: "Fizer",
     price: "25 T",
+    category: "mocktail",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799245/WhatsApp_Image_2021-06-04_at_14.00.32_bucfil.jpg",
   },
   {
     name: "Mohito",
     price: "25 T",
+    category: "maoktail",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799245/WhatsApp_Image_2021-06-04_at_14.00.34_1_vngsqj.jpg",
   },
   {
     name: "Watermelone juice ",
     price: "24 T",
+    category: "smoothie",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799246/WhatsApp_Image_2021-06-04_at_14.00.35_fjehpo.jpg",
   },
   {
     name: "Adas",
     price: "28 T",
+    category: "smoothie",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799244/WhatsApp_Image_2021-06-04_at_14.00.33_rfu9x4.jpg",
   },
   {
     name: "Red brew",
     price: "25 T",
+    category: "mocktail",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799244/WhatsApp_Image_2021-06-04_at_14.00.32_1_nlot6j.jpg",
   },
@@ -81,6 +94,7 @@ export const menuItem = [
   {
     name: "Cold brew",
     price: "22 T",
+    category: "coldei_based_coffee",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799241/WhatsApp_Image_2021-06-04_at_14.00.30_1_u9qzuq.jpg",
   },
@@ -88,6 +102,7 @@ export const menuItem = [
   {
     name: "Chocolate shake",
     price: "27 T",
+    category: "shake",
     image:
       "https://res.cloudinary.com/bonte/image/upload/v1622799241/WhatsApp_Image_2021-06-04_at_14.00.23_zlalce.jpg",
   },
