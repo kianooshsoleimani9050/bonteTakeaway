5669e67f4bfd2b7102ea88ecce9df491bc23e845
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-06 00:49:16 +0430

    making about page and component

--- src/comp/about.jsx
+++ src/comp/about.jsx
@@ -1,7 +1,14 @@
+import { about } from "../utils/about";
 const AboutPage = () => {
+  console.log(about);
+  console.log(about.about_section.header);
   return (
-    <div>
-      <h5>about us</h5>
+    <div className="p-0 m-0 d-flex flex-row justify-content-center align-items-center">
+      <div>door image goes here</div>
+      <div className="p-0 m-0 w-75 d-flex flex-column justify-content-center align-items-center">
+        <h1 className="about-header p-0 m-0">{about.about_section.header}</h1>
+        <strong className="p-0 m-0 w-75">{about.about_section.content}</strong>
+      </div>
     </div>
   );
 };
