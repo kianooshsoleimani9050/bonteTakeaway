b9af030e48217bda8f94b02aa2b638e2977022e6
kianooshsoleimani9050 <mazyar.solo@gmail.com>
2021-06-06 00:54:29 +0430

    add scroll to top bottom

--- src/comp/menu.jsx
+++ src/comp/menu.jsx
@@ -1,8 +1,21 @@
 import { useState } from "react";
 import { menuItem } from "../utils/menu_items";
+import { FaArrowCircleUp } from "react-icons/fa";
 const Menu = () => {
   // console.log(menuItem);
   const [menu, setMenu] = useState(menuItem);
+  const [showScrollBtn, setShowScrollBtn] = useState(true);
+  const scrollTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+  const activeScrollButton = () => {
+    if (window.pageYOffset > 150) {
+      setShowScrollBtn(true);
+    } else if (window.pageYOffset < 150) {
+      setShowScrollBtn(false);
+    }
+  };
+  window.addEventListener("scroll", activeScrollButton);
   return (
     <div className="menu p-1 m-0 d-flex flex-column justify-content-start align-items-center">
       <ul className="filter-section p-2 m-0 mt-2 d-flex row justify-content-start align-items-center ">
@@ -105,14 +118,23 @@ const Menu = () => {
               key={item.name}
               className="p-2 m-0 col-md-3 col-sm-6 col-12 rounded"
             >
-              <div className="p-0 m-0 ">
+              <div className="p-0 m-0 menu-img-container">
                 <img
-                  className="img-fluid p-0 m-0 w-100"
+                  className="img-fluid p-0 m-0 w-100 menu-img"
                   src={item.image}
                   alt="bonte-item"
                 />
+                <div className="dec-container">
+                  <div className="">
+                    <h4>{item.decription}</h4>
+                  </div>
+                </div>
               </div>
-              <div className="item-card p-3 m-0 mt-2 rounded d-flex justify-content-between">
+              <div className="item-card-hanger d-flex justify-content-between align-items-center">
+                <div className="hanger-left"></div>
+                <div className="hanger-right"></div>
+              </div>
+              <div className="item-card p-3 m-0  rounded d-flex justify-content-between">
                 <div className="p-1 m-0">
                   <label>Name: </label>
                   <h4>{item.name}</h4>
@@ -126,6 +148,10 @@ const Menu = () => {
           );
         })}
       </div>
+      <FaArrowCircleUp
+        className={`scroll-to-top-icon ${showScrollBtn ? "" : "d-none"}`}
+        onClick={scrollTop}
+      />
     </div>
   );
 };
